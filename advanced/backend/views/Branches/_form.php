9dfdd264b6df434053e1b67bf44dc0bb3df00dcf
e <email>
2017-11-07 19:38:29 +0800

    第十四节，在branches 表单中使用了 select2 扩展

--- advanced/backend/views/Branches/_form.php
+++ advanced/backend/views/Branches/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use backend\models\Companies;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Branches */
@@ -16,9 +17,19 @@ use backend\models\Companies;
 
     <?= $form->field($model, 'companies_company_id')->dropDownList(
         ArrayHelper::map( Companies::find()->all(),'company_id', 'company_name'),
-        ['prompt' => 'Select Company']  //下拉框写法
-    ) ?>
-
+        ['prompt' => 'Select Company'] //下拉框写法
+      ) ?>
+    <!--select 2 使用开始-->
+    <?= $form->field($model, 'companies_company_id')->widget(Select2::classname(), [
+        'data' => ArrayHelper::map( Companies::find()->all(),'company_id', 'company_name'),
+        'language' => 'en',
+        'options' => ['placeholder' => 'Select a company ...'],
+        'pluginOptions' => [
+            'allowClear' => true
+        ],
+    ]);?>
+    <!--select 2 使用结束-->
+    
     <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'branch_address')->textInput(['maxlength' => true]) ?>
