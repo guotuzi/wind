795246b17746cc64b5c60845b90d40dde7da880c
e <email>
2017-11-07 19:08:39 +0800

    第十三节，在module ——里面使用了 2amigos/yii2-date-picker-widget

--- advanced/backend/modules/settings/views/Companies/_form.php
+++ advanced/backend/modules/settings/views/Companies/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model backend\modules\settings\models\Companies */
@@ -17,6 +18,20 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'company_email')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'company_address')->textInput(['maxlength' => true]) ?>
+
+    <!--    此处使用了 yii2 的扩展功能，日期获得，datetimepicker-->
+    <?= $form->field($model, 'company_start_date')->widget(DatePicker::className(), [
+        // inline too, not bad
+//        'inline' => false,
+        // modify template for custom rendering
+//        'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+        'clientOptions' => [
+            'minView' => "month",   //选择日期后，不会再跳转去选择时分秒
+            'autoclose' => true,
+            'format' => 'yyyy-m-dd'
+        ]
+    ]);?>
+    <!--日期扩展结束-->
 
     <?= $form->field($model, 'company_created_date')->textInput() ?>
 
